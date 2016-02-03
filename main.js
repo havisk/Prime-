df47ead027abf27e86de68c9f4329928bd478836
Kool Havis <havisk@gmail.com>
2016-02-03 15:25:00 -0500

    more progress

--- main.js
+++ main.js
@@ -1,44 +1,62 @@
-function secret(num){
-  var primes = [];
-  var D = [];
-  function sieve(num) {
-    //gets all primes of number
-    for (var q = 2; q < num; q++) 
-      {
-          if (D[q]) 
-          {
-                  for (var i =0 ; i < D[q].length; i++) 
-                  {
-                     var p = D[q][i];
-                     if (D[p+q]) D[p+q].push(p);
-                     else D[p+q] = [p];
-                  }
-                  delete D[q];
-          } else {
-                  primes.push(q);
-                  if (q*q < num) D[q*q] = [q];
-          }
-      }
-  
-    function checkPrime(primes) {
 
-      var test1 = secret(x) + secret(y);
-      var test2 = secret(x + y);
+function secret(){
+  var n = prompt("Please enter number");
+  // console.log(n)
+  var num = parseInt(n);
+  return num;
 
-      for (var x = 0; x < primes.length; x++) {
-        for (var y = 0; y < primes.length; y++) {
-          if (test1 === test2)
-          {
-            print("Is additive")
-          }else{
-            print("Not additive")
-          }
-       }
-      }
-    }
- return primes;
-  }
- return num;
 }
+
+function sieve() {
+  //gets all primes of number
+  var D = [];
+  var primes = [];
+  var num = secret();
+
+  console.log(num);
+
+  for (var q = 2; q < num; q++) 
+    {
+        if (D[q]) 
+        {
+                for (var i=0 ; i< D[q].length; i++) 
+                {
+                   var p = D[q][i];
+                   if (D[p+q]) D[p+q].push(p);
+                   else D[p+q] = [p];
+                }
+                delete D[q];
+        } else {
+                primes.push(q);
+                if (q*q < num) D[q*q] = [q];
+        }
+    }
+    return primes;
+}
+
+function checkPrime(){
+  var test1 = [secret(x) + secret(y)];
+  var test2 = [secret(x + y)];
+  var primes = sieve();
+  console.log(primes);
+
+  for (var x=0; x<primes.length; x++) {
+    for (var y=0; y<primes.length; y++) {
+
+      console.log("x equals" + [x]);
+      console.log("y equals" + [y]);
+
+      if (test1 === test2)
+      {
+        return("Is additive")
+      }else{
+        return("Not additive")
+      }
+   }
+ }
+
+}
+checkPrime();
+
 
 
